09df604420218aed33264e0f10d98f7ab64e048f
androidparanoidal <veiur@yandex.com>
2023-08-14 10:45:08 +0300

    Fixed bugs

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -6,9 +6,7 @@ import 'package:weather_app/widgets/weather_card.dart';
 import 'package:weather_app/widgets/wind_humidity_card.dart';
 import '../api/weather_api.dart';
 import '../model/weather_model.dart';
-import 'package:weather_app/forecast_util.dart';
-
-import '../widgets/city_display_widget.dart';
+import 'city_search_screen.dart';
 import 'login_screen.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -35,6 +33,57 @@ class _WeatherScreenState extends State<WeatherScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        leading: Row(),
+        actions: [
+          Row(
+            children: [
+              SizedBox(
+                height: 24.0,
+                width: 24.0,
+                child: IconButton(
+                    onPressed: () async {
+                      var tappedName = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CitySearchScreen()));
+                      if (tappedName != null) {
+                        setState(() {
+                          _cityName = tappedName;
+                          forecastObject = WeatherAPI()
+                              .getWeather(cityName: _cityName, isCity: true);
+                        });
+                      }
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    icon: Icon(Icons.search, color: Colors.white)),
+              ),
+              SizedBox(width: 50),
+              SizedBox(
+                height: 24.0,
+                width: 24.0,
+                child: IconButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ));
+                  },
+                  padding: EdgeInsets.all(0.0),
+                  icon: Icon(Icons.logout_outlined, color: Colors.white),
+                ),
+              ),
+              SizedBox(width: 15),
+            ],
+          )
+        ],
+      ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -54,7 +103,31 @@ class _WeatherScreenState extends State<WeatherScreen> {
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(top: 15),
-                          child: CityView(snapshot: snapshot),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    height: 24.0,
+                                    width: 24.0,
+                                    child: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          forecastObject =
+                                              WeatherAPI().getWeather();
+                                        });
+                                      },
+                                      padding: EdgeInsets.all(0.0),
+                                      icon: Icon(Icons.location_on_outlined,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  CityView(snapshot: snapshot),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                         WeatherCard(snapshot: snapshot),
                         HourlyWeatherCard(snapshot: snapshot),
@@ -63,10 +136,16 @@ class _WeatherScreenState extends State<WeatherScreen> {
                     );
                   } else {
                     return Center(
-                      child: Text(
-                        'City not found\nplease enter correct city name',
-                        style: TextStyle(fontSize: 25),
-                        textAlign: TextAlign.center,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 100),
+                          Text(
+                            'Город не найден!\nВведите корректное название.',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                          //SizedBox(height: 30),
+                        ],
                       ),
                     );
                   }
--- lib/widgets/city_view.dart
+++ lib/widgets/city_view.dart
@@ -1,10 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../api/weather_api.dart';
-import '../forecast_util.dart';
 import '../model/weather_model.dart';
-import '../screens/city_search_screen.dart';
-import '../screens/login_screen.dart';
 
 // the top widget
 class CityView extends StatefulWidget {
@@ -18,7 +13,6 @@ class _CityViewState extends State<CityView> {
   @override
   Widget build(BuildContext context) {
     late Future<WeatherModel> forecastObject;
-    late String _cityName;
 
     var city = widget.snapshot.data?.city?.name;
     var country = widget.snapshot.data?.city?.country;
@@ -28,19 +22,6 @@ class _CityViewState extends State<CityView> {
         children: [
           Row(
             children: [
-              SizedBox(
-                height: 24.0,
-                width: 24.0,
-                child: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      forecastObject = WeatherAPI().getWeather();
-                    });
-                  },
-                  padding: EdgeInsets.all(0.0),
-                  icon: Icon(Icons.location_on_outlined, color: Colors.white),
-                ),
-              ),
               Text(
                 '$city, $country',
                 style: TextStyle(
@@ -48,43 +29,6 @@ class _CityViewState extends State<CityView> {
               ),
             ],
           ),
-
-          // SizedBox(
-          //   height: 24.0,
-          //   width: 24.0,
-          //   child: IconButton(
-          //       onPressed: () async {
-          //         var tappedName = await Navigator.push(
-          //             context,
-          //             MaterialPageRoute(
-          //                 builder: (context) => CitySearchScreen()));
-          //         if (tappedName != null) {
-          //           setState(() {
-          //             _cityName = tappedName;
-          //             forecastObject = WeatherAPI()
-          //                 .getWeather(cityName: _cityName, isCity: true);
-          //           });
-          //         }
-          //       },
-          //       padding: EdgeInsets.all(0.0),
-          //       icon: Icon(Icons.search, color: Colors.white)),
-          // ),
-          SizedBox(
-            height: 24.0,
-            width: 24.0,
-            child: IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return LoginScreen();
-                  },
-                ));
-              },
-              padding: EdgeInsets.all(0.0),
-              icon: Icon(Icons.logout_outlined, color: Colors.white),
-            ),
-          )
         ],
       ),
     );
